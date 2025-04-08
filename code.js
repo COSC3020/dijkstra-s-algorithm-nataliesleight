c220c4972eaee207e8a88d4fada3714e373ac0ff
nataliesleight <nsleight@uwyo.edu>
2025-04-07 20:35:57 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,54 @@
 function dijkstra(graph, sourceNode) {
-    return [];
+    // adjacency matrix
+    // graph is 2d array and sourcenode is an integer index
+    if (graph.length == 0) {return [];}
+    
+    // create distance array
+    var verNum = graph.length;
+    var distArr = new Array(verNum);
+    for (var i = 0; i < verNum; i++) {
+        distArr[i] = [Infinity, false];
+    }
+
+    // make sourcenode distance zero
+    distArr[sourceNode] = [0, false];
+
+    // look through array for smallest key value
+    var hasFalse = true;
+    while (hasFalse == true) {
+        hasFalse = false;
+        var smallestValue = Infinity;
+        for (var i = 0; i < verNum; i++) {
+            var svIndex;
+            if (distArr[i][1] == false) {
+                hasFalse = true;
+                if (distArr[i][0] < smallestValue) {
+                    smallestValue = distArr[i][0];
+                    svIndex = i;
+                }
+            }
+        }
+        if (hasFalse == false) {break;}
+        
+        // set distance-true
+        distArr[svIndex][1] = true;
+
+
+        // look at all outgoing vertices/edges
+        for (var i = 0; i < verNum; i++) {
+            if (graph[svIndex][i] != 0) {                                           // looking through proper row, found edge connecting to i
+                if (distArr[svIndex][0] + graph[svIndex][i] < distArr[i][0]) {      // if dist to first vertex plus dist to next vertex less than current dist
+                    distArr[i][0] = distArr[svIndex][0] + graph[svIndex][i];
+                }
+            }
+        }
+    }
+
+    // create single dimensional array to return
+    var finalArr = new Array(verNum);
+    for (var i = 0; i < verNum; i++) {
+        finalArr[i] = distArr[i][0];
+    }
+
+    return finalArr;
 }
