9c9c68333eb70f3a50583ffb6f54d9a784879b58
nataliesleight <nsleight@uwyo.edu>
2025-04-09 15:58:39 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -21,15 +21,15 @@ var testGraph2 = [
 var result = dijsktra(testGraph2,3);
 jsc.assert(JSON.stringify(result) == JSON.stringify([6,5,3,0]));
 
-var testGraph3 = [
-  [0,3,0,0],
-  [3,0,1,2],
-  [0,1,0,4],
-  [0,2,4,0]
+var testGraph3 = [ // this is the graph with an isloated node
+  [0,0,1,3],
+  [0,0,0,0],
+  [0,0,0,1],
+  [0,0,1,0]
   ];
 
-var result = dijsktra(testGraph3,1);
-jsc.assert(JSON.stringify(result) == JSON.stringify([3,0,1,2]));
+var result = dijsktra(testGraph3,0);
+jsc.assert(JSON.stringify(result) == JSON.stringify([0,Infinity,1,2]));
 
 var testGraph4 = [
   [0,2,0,1,0],
